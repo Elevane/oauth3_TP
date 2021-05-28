4e1e7bbf646db030ba2817be7f951a9fd39ff8df
bastien aubry <pma.bastien.aubry@gmail.com>
2021-05-28 17:56:20 +0200

    init db

---
+++ Models/User.py
@@ -0,0 +1,35 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from main import db
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=True, nullable=False)
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.email
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
+from Models.User import *
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+from flask_login import login_user, logout_user, login_required, LoginManager
 
 app = Flask(__name__)
-db = SQLAlchemy()
-db.init_app(app)
+login_manager = LoginManager()
 app.config['SECRET_KEY'] = "mouahahaahahaah"
 app.config["SQLALCHEMY_DATABASE_URI"]  = 'mysql://root:@127.0.0.1:3306/oauth'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+login_manager.init_app(app)
+errors = ""
 
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
 
@@ -22,11 +26,39 @@ class LoginForm(FlaskForm):
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('/home')
+        username = request.form["username"]
+        password = request.form["password"]
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            print("user")
+            redirect("home")
+        else:
+            errors ={
+                "message": "utilisatieur introuvable"
+            }
+            return render_template('login.html', title='Se connecter', form=form, errors=errors)
     return render_template('login.html', title='Se connecter', form=form)
 
 
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/login")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
+@login_manager.unauthorized_handler
+def need_to_be_logged():
+    return  redirect('/login')
+
+
 @app.route("/home")
+@login_required
 def index():
     user = {"name": "gaetan", "password": "password"}
     return render_template('index.html', title='Index', user=user)
