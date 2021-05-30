7a8384888ec0382e71e918d7250acda3f06f8978
Bastien <pma.Bastien.aubry@gmail.com>
2021-05-30 11:32:50 +0200

    authentification ok

--- main.py
+++ main.py
@@ -1,37 +1,65 @@
+import os
+
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from Models.User import *
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-from flask_login import login_user, logout_user, login_required, LoginManager
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import ValidationError
 
+from Forms.LoginForm import LoginForm
+from flask_login import logout_user, login_required, LoginManager, login_user
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 login_manager = LoginManager()
 app.config['SECRET_KEY'] = "mouahahaahahaah"
-app.config["SQLALCHEMY_DATABASE_URI"]  = 'mysql://root:@127.0.0.1:3306/oauth'
+app.config["SQLALCHEMY_DATABASE_URI"]  = 'sqlite:///' + os.path.join(basedir, 'auth.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager.init_app(app)
 errors = ""
 
 
-class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Sign In')
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(30), unique=True)
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.id
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        username = request.form["username"]
+        email = request.form["email"]
         password = request.form["password"]
-        user = User.query.filter_by(username=username, password=password).first()
+        user = User.query.filter_by(email=email, password=password).first()
         if user:
-            print("user")
-            redirect("home")
+            login_user(user)
+            return redirect("/home")
+
         else:
             errors ={
                 "message": "utilisatieur introuvable"
@@ -40,16 +68,39 @@ def login():
     return render_template('login.html', title='Se connecter', form=form)
 
 
+@app.route("/create", methods=['GET', 'POST'])
+def create():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = request.form["email"]
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email address already exists')
+        else :
+            user = User(email=email, password=password)
+            db.session.add(user)
+            db.session.commit()
+            redirect("login")
+    return render_template("create.html", form=form, title="creer un compte")
+
+
+
+@app.route("/home")
+@login_required
+def index():
+    return render_template('index.html', title='Index')
+
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect("/login")
+    return redirect("/login" ,)
 
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    return User.query.get(user_id)
 
 
 @login_manager.unauthorized_handler
@@ -57,11 +108,7 @@ def need_to_be_logged():
     return  redirect('/login')
 
 
-@app.route("/home")
-@login_required
-def index():
-    user = {"name": "gaetan", "password": "password"}
-    return render_template('index.html', title='Index', user=user)
+
 
 
 if __name__ =="__main__":
